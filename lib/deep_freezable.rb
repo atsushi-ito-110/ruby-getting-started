8045dad0a0f0f3f9f51ba86436477f1d2d895279
Ito <atsushi.itoh66@gmail.com>
2020-07-06 03:26:52 +0900

    update: Model answer

--- lib/deep_freezable.rb
+++ lib/deep_freezable.rb
@@ -1,13 +1,14 @@
 module DeepFreezable
   def deep_freeze(array_or_hash)
-    case array_or_hash.class.to_s
-    when 'Array' then
+    case array_or_hash
+    when Array then
       array_or_hash.each do |element|
         element.freeze
       end
-    when 'Hash' then
+    when Hash then
       array_or_hash.each do |key, value|
-        [key.freeze, value.freeze].freeze
+        key.freeze
+        value.freeze
       end
     else
       p array_or_hash.class
